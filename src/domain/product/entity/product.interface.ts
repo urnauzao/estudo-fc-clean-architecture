c175ef71605133fac0628b95210d6d817b97bb88
Urnau <julio.cesar.08.08.93@gmail.com>
2023-03-04 20:52:44 -0300

    Patter Notification implements on Products with test

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -1,7 +1,10 @@
+import Notification from "../../@shared/notification/notification";
+
 export default interface ProductInterface {
   get id(): string;
   get name(): string;
   get price(): number;
+  get notification(): Notification;
   changeName(name: string): void;
   changePrice(price: number): void;
 }
--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,15 +1,21 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
-export default class Product implements ProductInterface {
-  private _id: string;
+export default class Product extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   get id(): string {
@@ -33,17 +39,8 @@ export default class Product implements ProductInterface {
     this._price = price;
     this.validate();
   }
-
-  validate(): boolean {
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
-    }
-    if (this._name.length === 0) {
-      throw new Error("Name is required");
-    }
-    if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
-    }
-    return true;
+  
+  validate() {
+    ProductValidatorFactory.create().validate(this);
   }
 }
